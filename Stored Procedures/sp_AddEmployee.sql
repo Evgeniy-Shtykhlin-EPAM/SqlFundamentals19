39b3a9b51c6ce3a9fd566378c9ac972dc7222e12
Evgeniy-Shtykhlin-EPAM <Evgeniy_Shtykhlin@epam.com>
2023-07-09 22:46:31 +0600

    Changes in view and stored procerdure

--- Stored Procedures/sp_AddEmployee.sql
+++ Stored Procedures/sp_AddEmployee.sql
@@ -9,37 +9,41 @@
     @State nvarchar(20)= NULL,
     @ZipCode nvarchar(20)= NULL
 AS
+    DECLARE @Tempcompany NVARCHAR(20)=@CompanyName
+
     if 
         (SELECT TRIM(@EmployeeName) AS TrimmedString) is null
     AND (SELECT TRIM(@FirstName) AS TrimmedString) is null
     AND (SELECT TRIM(@LastName) AS TrimmedString) is null
     BEGIN  
-       PRINT 'Name or FirstName or Last name should be fill'  
+       PRINT 'EmployeeName or FirstName or LastName should be fill'
     END
+     else
+        BEGIN
+            if  
+                (SELECT TRIM(@FirstName) AS TrimmedString) is not null
+                OR (SELECT TRIM(@LastName) AS TrimmedString) is not null
+             BEGIN
 
-    else if(len(@CompanyName)>20)
-    BEGIN  
-    DECLARE @Tempcompany NVARCHAR(20)=@CompanyName
-        INSERT into Employee(EmployeeName,Position,CompanyName)
-        values
-        (@EmployeeName,@Position,@Tempcompany)
-        INSERT into Person(FirstName,LastName)
-        values
-        (@FirstName,@LastName)
-        INSERT into Address(Street,City,State,ZipCode)
-        values
-        (@Street,@City,@State,@ZipCode)
-    END
+                INSERT into Person(FirstName,LastName)
+                values
+                (@FirstName,@LastName)
 
-    else
-    BEGIN  
-        INSERT into Employee(EmployeeName,Position,CompanyName)
-        values
-        (@EmployeeName,@Position,@CompanyName)
-        INSERT into Person(FirstName,LastName)
-        values
-        (@FirstName,@LastName)
-        INSERT into Address(Street,City,State,ZipCode)
-        values
-        (@Street,@City,@State,@ZipCode)
-    END
+                INSERT into Address(Street,City,State,ZipCode)
+                values
+                (@Street,@City,@State,@ZipCode)
+
+                
+
+                INSERT into Employee(AddressId,PersonId,EmployeeName,Position,CompanyName)
+                values
+                ((select top(1) a.Id from Address as a 
+                where a.Street=@Street and a.City=@City and a.State=@State and a.ZipCode=@ZipCode),
+                (select top(1) p.Id from Person as p
+                where p.FirstName=@FirstName and p.LastName=@LastName),
+                @EmployeeName,@Position,@Tempcompany);
+
+             END
+             else
+                PRINT 'Cannot create Person First Name and Last Name is absent'
+        END
